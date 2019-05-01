980a9afabb11f915b5a614c4d11c3ed51ab375f7
James Britton <brittonjp@gmail.com>
2019-05-01 14:53:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(array_dwarves)
 end
 
 def summon_captain_planet(array_calls)
-  
+  array_calls.map do |call|
+    word.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
