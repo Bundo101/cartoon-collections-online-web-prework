33b41fe413ccb21bcd4c4fa4dcca679975d0cf13
James Britton <brittonjp@gmail.com>
2019-05-01 15:00:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(array_calls)
 end
 
 def long_planeteer_calls(array_calls)
-  array_calls.any? do
-
+  array_calls.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
