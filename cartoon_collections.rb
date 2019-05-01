7ecc299e32a8dac99346599eb7e3c6f71e04f368
James Britton <brittonjp@gmail.com>
2019-05-01 15:01:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array_calls)
 end
 
 def long_planeteer_calls(array_calls)
-  array_calls.include? do |call|
+  array_calls.any? do |call|
     call.length > 4
   end
 end
